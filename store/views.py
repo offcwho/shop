669c29930386b8eafc27512d9f401c4652646773
offcwho <nhnhnhbgbg@gmail.com>
2025-01-17 18:17:50 +0300

    upd

--- store/views.py
+++ store/views.py
@@ -5,6 +5,6 @@ from store.models import Product
 
 # Create your views here.
 def index(request):
-    products = Product.objects.all()
+    products = Product.objects.filter(is_active=True).order_by('-id')
     context = {'products': products}
     return render(request, 'index.html', context)
